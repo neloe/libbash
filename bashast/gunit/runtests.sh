0379be89818dd826e8f15d4c3b33acf0da39d396
Nathan Eloe <powerofazure@gmail.com>
2010-05-29 11:12:43 -0500

    Added GPL-2 license

--- bashast/gunit/runtests.sh
+++ bashast/gunit/runtests.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#############################################
+#Copyright 2010 Nathan Eloe
+#
+#This file is part of libbash.
+#
+#libbash is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 2 of the License, or
+#(at your option) any later version.
+#
+#libbash is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with libbash.  If not, see <http://www.gnu.org/licenses/>.
+##############################################
+
 #first, set up the classpath
 if type -p java-config > /dev/null; then
 	export CLASSPATH=".:$(java-config -dp antlr-3)"
