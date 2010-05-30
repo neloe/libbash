fdbd747943414569432b8e2fd33fd0c3c465f41b
Nathan Eloe <powerofazure@gmail.com>
2010-05-29 19:12:32 -0500

    Test Framework Update
    Tests can be chosen via the command line:
    	cd gunit
    	./runtests.sh <testfile_name>*
    If no test file names are given, runs all test files in the directory
    otherwise, runs all test files in the provided list

--- bashast/gunit/runtests.sh
+++ bashast/gunit/runtests.sh
@@ -16,6 +16,29 @@
 #You should have received a copy of the GNU General Public License
 #along with libbash.  If not, see <http://www.gnu.org/licenses/>.
 ##############################################
+#variable for number of tests
+let failedtests=0
+
+#test running function
+function rtest {
+	grammar=`cat $1 |grep gunit|awk '{print $2}'|awk -F';' '{print $1}'`
+		grammar="$grammar.g"
+		echo "Running unit tests for: $grammar"
+		cp ../$grammar .
+		java org.antlr.Tool $grammar
+		javac *.java
+		java org.antlr.gunit.Interp $gtest > $grammar.output
+		failed=`cat $grammar.output|grep "Failures:"|awk -F: '{print $3}'|awk '{print $1}'`
+	
+		if [[ failed -ne '0' ]]; then
+			echo "Test failed"
+			let failedtests=failedtests+1
+		else
+			echo "Test Passed"
+			rm $grammar.output
+		fi
+		rm *.java *.class *.g *.tokens
+}
 
 #first, set up the classpath
 if type -p java-config > /dev/null; then
@@ -23,27 +46,20 @@ if type -p java-config > /dev/null; then
 else
 	export CLASSPATH=".:/usr/share/java/antlr-3.2.jar:/Users/squishy/ANTLR/antlr.jar"
 fi
-let failedtests=0
-for gtest in `ls |grep gunit`; do
-	grammar=`cat $gtest |grep gunit|awk '{print $2}'|awk -F';' '{print $1}'`
-	grammar="$grammar.g"
-	echo "Running unit tests for: $grammar"
-	cp ../$grammar .
-	java org.antlr.Tool $grammar
-	javac *.java
-	java org.antlr.gunit.Interp $gtest > $grammar.output
-	failed=`cat $grammar.output|grep "Failures:"|awk -F: '{print $3}'|awk '{print $1}'`
 
-	if [[ failed -ne '0' ]]; then
-		echo "Test failed"
-		let failedtests=failedtests+1
-	else
-		echo "Test Passed"
-		rm $grammar.output
-	fi
-	rm *.java *.class *.g *.tokens
-done
-
+if [[ $# -eq 0  ]]; then
+	for gtest in `ls |grep gunit`; do
+		rtest $gtest
+	done
+else
+	for gtest in "$@"; do
+		if [[ -a $gtest ]]; then
+			rtest $gtest
+		else
+			echo "gunit file not found: $gtest"
+		fi
+	done
+fi
 cat *.output 2> /dev/null
 rm *.output 2> /dev/null
 
