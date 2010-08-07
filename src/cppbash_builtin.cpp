e9458d962c5db7ad2533ab15c25b70b3fb7d5522
Nathan Eloe <powerofazure@gmail.com>
2010-08-07 11:03:35 -0500

    Virtual class for builtins
    Implements a virtual class to inherit builtin functions from.
    Allows the build system to create the shared library.

---
+++ src/cppbash_builtin.cpp
@@ -0,0 +1,29 @@
+/*
+Copyright 2010 Nathan Eloe
+
+This file is part of libbash.
+
+libbash is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+libbash is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libbash.  If not, see <http://www.gnu.org/licenses/>.
+*/
+///
+/// \file cppbash_builtin.cpp
+/// \author Nathan Eloe
+/// \brief Implementation of class to inherit builtins from
+///
+
+#include "cppbash_builtin.h"
+
+cppbash_builtin::cppbash_builtin(std::ostream &outstream, std::ostream &errstream, std::istream &instream): _out_stream(&outstream), _err_stream(&errstream), _inp_stream(&instream)
+{
+}
+
